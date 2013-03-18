7f29deb09d5e8018286215bac6ed1a24b83598e0
KUOKA Yusuke <ykuoka@gmail.com>
2013-03-18 13:42:46 +0900

    Added examples for mocking vals and lazy vals

--- 1.9/src/test/scala/Spec1.scala
+++ 1.9/src/test/scala/Spec1.scala
@@ -1,7 +1,8 @@
 import org.specs2.matcher.DataTables
+import org.specs2.mock.Mockito
 import org.specs2.mutable._
 
-object Spec1 extends Specification with DataTables {
+object Spec1 extends Specification with DataTables with Mockito {
 
   "Foo" should {
     "bar" in {
@@ -18,6 +19,52 @@ object Spec1 extends Specification with DataTables {
           else
             num must equalTo (num)
       }
+    }
+  }
+
+  "Mockito" in {
+    "mock vals and lazy vals" in {
+      trait Foo {
+        val src: Int
+        private val prv: Int = src + 1
+        private def transform(n: Int) = n.toString
+        lazy val myLazyVal = transform(prv)
+        val myVal = prv.toString
+      }
+      val foo = mock[Foo]
+      foo.src returns 1
+      foo.myLazyVal returns "3"
+      foo.myVal returns "4"
+      foo.myLazyVal must be equalTo ("3")
+      foo.myVal must be equalTo ("4")
+      foo.myVal must be equalTo ("4")
+
+      case class Bar(src: Int) extends Foo
+      val bar = mock[Bar]
+      bar.src returns 1
+      bar.myLazyVal returns "3"
+      bar.myLazyVal must be equalTo ("3")
+      bar.myVal returns "3"
+      bar.myVal must be equalTo ("3")
+
+      trait Baz {
+        val src: Int
+        private val prv: Int = src + 1
+        private def transform(n: Int) = n.toString
+        lazy val myLazyVal2 = transform(prv)
+        val myVal2 = prv.toString
+      }
+      case class Hoge(src: Int) extends Foo with Baz
+      val hoge = mock[Hoge]
+      hoge.src returns 1
+      hoge.myLazyVal returns "3"
+      hoge.myLazyVal must be equalTo ("3")
+      hoge.myVal returns "3"
+      hoge.myVal must be equalTo ("3")
+      hoge.myLazyVal2 returns "3"
+      hoge.myLazyVal2 must be equalTo ("3")
+      hoge.myVal2 returns "3"
+      hoge.myVal2 must be equalTo ("3")
     }
   }
 }
